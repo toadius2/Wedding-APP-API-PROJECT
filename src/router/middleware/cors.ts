d19135c1ab049a993d2697c83208a8476f3e72c6
Alexander Engel <ae94@icloud.com>
2020-11-02 12:48:25 -0800

    cors fix

--- src/router/middleware/cors.ts
+++ src/router/middleware/cors.ts
@@ -2,9 +2,10 @@ import * as express from "express"
 import { APIRequest, APIResponse } from "../basicrouter"
 
 export default function applyCORS(req: APIRequest, res: APIResponse, next: express.NextFunction) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.headers['origin'] ? (req.headers['origin'] as string) : '*');
     res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, User-Agent");
+    res.header("Access-Control-Allow-Credentials", "true")
     if (req.method == "OPTIONS") {
         res.jsonContent({})
     } else
